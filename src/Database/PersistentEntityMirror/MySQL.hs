d8c1ca2af5691107d9706537868d5e6a3014bb45
Ryan Mulligan <ryan@ryantm.com>
2015-09-19 19:55:32 -0700

    specify and implement descriptionOfDatabase better

--- src/Database/PersistentEntityMirror/MySQL.hs
+++ src/Database/PersistentEntityMirror/MySQL.hs
@@ -6,6 +6,7 @@ import qualified Data.ByteString as BS
 import           Data.ByteString.Builder
 import           Data.HashMap.Strict as HM
 import           Data.Text
+import           Data.Text.Encoding (encodeUtf8)
 import           Database.MySQL.Simple
 import           Database.MySQL.Simple.Param
 
@@ -20,7 +21,7 @@ type MySQLDescribe = (
   )
 
 
-type DatabaseDescription = HM.HashMap String [MySQLDescribe]
+type DatabaseDescription = HM.HashMap Text [MySQLDescribe]
 
 
 -- | Given a database name, returns a DatabaseDescription in IO
@@ -28,13 +29,16 @@ type DatabaseDescription = HM.HashMap String [MySQLDescribe]
 -- Warning: The caller is responsible for making sure the database
 -- name is properly formatted, that is, this function does not protect
 -- against SQL injection.
-descriptionOfDatabase :: String -> -- ^ a database name
+descriptionOfDatabase :: Text -> -- ^ a database name
                          IO DatabaseDescription
 descriptionOfDatabase database = do
   conn <- connect defaultConnectInfo {
-    connectDatabase = database }
-  tableDescription <- descriptionOfTable conn "GLOBAL_VARIABLES"
-  return (HM.singleton "GLOBAL_VARIABLES" tableDescription)
+    connectDatabase = unpack database }
+  tables <- tablesOfDatabase conn
+  keyValues <- mapM (\ table -> do
+    description <- descriptionOfTable conn table
+    return (table, description)) tables
+  return (HM.fromList keyValues)
 
 
 -- | Given a database connection, return a list of table names in that
@@ -53,7 +57,7 @@ tablesOfDatabase conn = do
 -- is properly formatted, that is, this function does not protect
 -- against SQL injection.
 descriptionOfTable :: Connection ->  -- ^ a mysql-simple connection
-                      String ->  -- ^ table name
+                      Text ->  -- ^ table name
                       IO [MySQLDescribe]
 descriptionOfTable conn table = do
-  query conn "describe ?" (Only (Plain (stringUtf8 table)))
+  query conn "describe ?" (Only (Plain (byteString (encodeUtf8 table))))
--- test/PersistentEntityMirror/MySQLSpec.hs
+++ test/PersistentEntityMirror/MySQLSpec.hs
@@ -36,6 +36,55 @@ globalVariablesDescription = [
   , Nothing
   , Just "")]
 
+{-
+MariaDB [information_schema]> describe schemata;
++----------------------------+--------------+------+-----+---------+-------+
+| Field                      | Type         | Null | Key | Default | Extra |
++----------------------------+--------------+------+-----+---------+-------+
+| CATALOG_NAME               | varchar(512) | NO   |     |         |       |
+| SCHEMA_NAME                | varchar(64)  | NO   |     |         |       |
+| DEFAULT_CHARACTER_SET_NAME | varchar(32)  | NO   |     |         |       |
+| DEFAULT_COLLATION_NAME     | varchar(32)  | NO   |     |         |       |
+| SQL_PATH                   | varchar(512) | YES  |     | NULL    |       |
++----------------------------+--------------+------+-----+---------+-------+
+5 rows in set (0.00 sec)
+-}
+
+
+schemataDescription :: [MySQLDescribe]
+schemataDescription = [
+  ( Just "CATALOG_NAME"
+  , Just "varchar(512)"
+  , Just "NO"
+  , Just ""
+  , Just ""
+  , Just ""),
+  ( Just "SCHEMA_NAME"
+  , Just "varchar(64)"
+  , Just "NO"
+  , Just ""
+  , Just ""
+  , Just ""),
+  ( Just "DEFAULT_CHARACTER_SET_NAME"
+  , Just "varchar(32)"
+  , Just "NO"
+  , Just ""
+  , Just ""
+  , Just ""),
+  ( Just "DEFAULT_COLLATION_NAME"
+  , Just "varchar(32)"
+  , Just "NO"
+  , Just ""
+  , Just ""
+  , Just ""),
+  ( Just "SQL_PATH"
+  , Just "varchar(512)"
+  , Just "YES"
+  , Just ""
+  , Nothing
+  , Just "")]
+
+
 spec :: Spec
 spec = do
   describe "mysql service dependency" ( do
@@ -57,7 +106,8 @@ spec = do
   describe "descriptionOfDatabase" (do
     it "should return a hashmap with keys as table names and values as lists of descriptions" (do
       result <- descriptionOfDatabase "information_schema"
-      result ! "GLOBAL_VARIABLES" `shouldBe` globalVariablesDescription))
+      result ! "GLOBAL_VARIABLES" `shouldBe` globalVariablesDescription
+      result ! "SCHEMATA" `shouldBe` schemataDescription))
   describe "tablesOfDatabase" (do
     specify "the information_schema should include GLOBAL_VARIABLES" (do
       conn <- connect defaultConnectInfo {
