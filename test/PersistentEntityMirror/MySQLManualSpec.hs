e212b19bfaa212a2aebb4a7ff25a7357cd058dd4
Ryan Mulligan <ryan@ryantm.com>
2015-09-29 07:13:44 -0700

    figured out how to test persistent template haskell manually

--- test/PersistentEntityMirror/MySQLManualSpec.hs
+++ test/PersistentEntityMirror/MySQLManualSpec.hs
@@ -1,14 +1,24 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 
 module PersistentEntityMirror.MySQLManualSpec where
 
 import Database.Persist.MySQL
+import Database.Persist.Quasi
 import Database.Persist.TH
 import Language.Haskell.TH.Quote
+import Data.Text
 
 import Test.Hspec
 
 
 description = "GlobalVariable sql=GLOBAL_VARIABLES\n  variableName Text sqltype=varchar(64)\n  variableValue Text sqltype=varchar(1024) Maybe default=Nothing\n"
 
-mkPersist sqlSettings (quoteExp persistLowerCase) description
+
+mkPersist sqlSettings (parse lowerCaseSettings "GlobalVariable sql=GLOBAL_VARIABLES\n  variableName Text sqltype=varchar(64)\n  variableValue Text sqltype=varchar(1024) Maybe default=Nothing\n")
+
+spec = undefined
